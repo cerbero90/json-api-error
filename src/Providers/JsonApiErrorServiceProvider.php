c267248de45637b5e9ceba21c18d527fbbca0278
Andrea Marco Sartori <andrea.marco.sartori@gmail.com>
2025-01-28 19:25:32 +0100

    Enforce JSON requests by default

--- src/Providers/JsonApiErrorServiceProvider.php
+++ src/Providers/JsonApiErrorServiceProvider.php
@@ -10,7 +10,7 @@ use Cerbero\JsonApiError\Exceptions\JsonApiException;
 use Cerbero\JsonApiError\JsonApiError;
 use Cerbero\JsonApiError\Services\TestResponseMixin;
 use Illuminate\Contracts\Debug\ExceptionHandler;
-use Illuminate\Http\JsonResponse;
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Testing\TestResponse;
@@ -35,11 +35,12 @@ final class JsonApiErrorServiceProvider extends ServiceProvider
      * Bootstrap the package services.
      *
      * @param \Illuminate\Foundation\Exceptions\Handler $handler
+     * @param \Illuminate\Foundation\Http\Kernel $kernel
      */
-    public function boot(ExceptionHandler $handler): void
+    public function boot(ExceptionHandler $handler, Kernel $kernel): void
     {
-        $handler->renderable(function (Throwable $e, Request $request): ?JsonResponse {
-            return JsonApiError::handlesRequest($request) ? JsonApiError::from($e)->response() : null;
+        $handler->renderable(function (Throwable $e, Request $request): ?JsonApiError {
+            return JsonApiError::handlesRequest($request) ? JsonApiError::from($e) : null;
         });
 
         $handler->reportable(function (JsonApiException|JsonApiErrorsAware|JsonApiSafe $e) use ($handler): bool {
@@ -49,6 +50,8 @@ final class JsonApiErrorServiceProvider extends ServiceProvider
 
             return false;
         });
+
+        $kernel->prependMiddlewareToGroup('api', JsonApiError::$middleware);
 
         $this->loadTranslationsFrom(self::PATH_LANG, self::NAME);
 
